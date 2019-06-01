8473f67dfadcf90e63d530892e69b489df6179fc
Facto <33882218+Sniped@users.noreply.github.com>
2019-06-01 00:48:45 -0400

    playing with x users

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,6 @@
 module.exports = {
     run: async (client) => {
         console.log(`Ready as ${client.user.tag}`);
+        client.user.setActivity(`with ${client.users.size} users`, { type: 'PLAYING' });
     }
 }
