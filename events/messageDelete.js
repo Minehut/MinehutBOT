4091e82e66ff94c0a6788f16745445657efce124
Facto <33882218+Sniped@users.noreply.github.com>
2019-06-19 19:21:22 -0400

    include attachments in edited and deleted messages

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,7 +1,8 @@
 module.exports = {
     run: async (client, msg) => {
         if (msg.channel.id != client.config.logchannel && !msg.content.includes('https://discord.gg' || 'http://discord.gg') && !msg.author.bot) {
-        client.log(`:wastebasket: ${msg.author.tag} (\`${msg.author.id}\`) message deleted in **#${msg.channel.name}**: (\`${msg.channel.id}\`)\n${client.replaceMentions(msg)}`);
+            const logmsg = await client.log(`:wastebasket: ${msg.author.tag} (\`${msg.author.id}\`) message deleted in **#${msg.channel.name}**: (\`${msg.channel.id}\`)\n${client.replaceMentions(msg)}`);
+            client.sendAttachments(msg, logmsg);
         } else return;
     }
 }
--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,7 +1,8 @@
 module.exports = {
     run: async (client, msgb, msga) => {
         if (msgb.embeds.length == 0 && msgb.content != msga.content && msga.channel.id && !msga.author.bot) {
-            client.log(`:pencil: ${msgb.author.tag} (\`${msgb.author.id}\`) message edited in **#${msgb.channel.name}**: \n**B**: ${client.replaceMentions(msgb)}\n**A**: ${await client.replaceMentions(msga)}`);
+            const logmsg = await client.log(`:pencil: ${msgb.author.tag} (\`${msgb.author.id}\`) message edited in **#${msgb.channel.name}**: \n**B**: ${client.replaceMentions(msgb)}\n**A**: ${await client.replaceMentions(msga)}`);
+            client.sendAttachments(msga, logmsg);
         }
     }
 }
