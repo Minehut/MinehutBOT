615555a04d432c3221cb073baede395de418ff7e
Facto <33882218+Sniped@users.noreply.github.com>
2019-06-20 22:19:28 -0400

    deleting tags and listing tags

---
+++ commands/tags.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+module.exports = {
+    run: async (client, msg, args) => {
+        const tags = await client.db.table('tags').run();
+        if (tags.length == 0) return msg.channel.send(':x: There are no tags!');
+        const tagnames = [];
+        tags.forEach(tag => {
+            tagnames.push(tag.id);
+        });
+        const embed = new Discord.RichEmbed()
+        .setDescription(`${tagnames.join('\n')}`)
+        .setColor('BLUE');
+        msg.channel.send(embed);
+    },
+    meta: {
+        aliases: ['tags'],
+        description: '',
+        permlvl: 1,
+        modCmd: false,
+        usage: ''  
+    }
+}
