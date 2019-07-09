60f289b3dade13de49155546bb36394d53be0a78
Facto <33882218+Sniped@users.noreply.github.com>
2019-07-09 11:45:58 -0400

    fix bypass of invite automod

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -8,6 +8,18 @@ module.exports = {
                 const logmsg = await client.log(`:pencil: ${msgb.author.tag} (\`${msgb.author.id}\`) message edited in **#${msgb.channel.name}**: \n**B**: ${client.replaceMentions(msgb)}\n**A**: ${await client.replaceMentions(msga)}`);
                 client.sendAttachments(msga, logmsg);    
             }
+            if (msga.content.includes('discord.gg')) {
+                const msgarray = msga.content.split(" ");    
+                const invitestring = msgarray.filter(element => {
+                    return element.includes('discord.gg');
+                });    
+                const invite = await client.fetchInvite(invitestring);
+                const invwl = await client.db.table('automodData').get('whitelisted-invites').run();
+                if (!invwl.invites.includes(invite.guild.id)) {
+                    msg.delete();
+                    client.log(`:no_entry_sign: censored message by ${msga.author.tag} (\`${msga.author.id}\`) in ${msga.channel.name} (\`${msga.channel.id}\`) invite \`${invite.code}\` to ${invite.guild.name}: \n${msga.content}`);
+                } else return;
+            }
         }
     }
 }
