5630e83dcce90fe5478f2c284a1d48180353d65d
Facto <33882218+Sniped@users.noreply.github.com>
2019-07-29 15:00:45 -0400

    events command and mban comand

---
+++ commands/events.js
@@ -0,0 +1,18 @@
+module.exports = {
+    run: async (client, msg, args) => {
+        const message = args.join(' ');
+        if (!message) return msg.channel.send(':x: You must supply a message to announce in the events channel!');
+        const events = client.channels.get(client.config.eventschannel);
+        const eventsrole = client.guilds.get(client.config.guilid).roles.find(r => r.name === 'Events');
+        await eventsrole.setMentionable(true);
+        await events.send(`${message}\n\n<@&${eventsrole.id}>`);
+        await eventsrole.setMentionable(false);
+    },
+    meta: {
+        aliases: ['events'],
+        description: 'Announce an event',
+        permlvl: 3,
+        modCmd: false,
+        usage: ''              
+    }
+}
