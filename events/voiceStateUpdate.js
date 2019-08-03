4dbe80a0bd2ecce4fce5cb8ae05440b98340fc3d
Facto <33882218+Sniped@users.noreply.github.com>
2019-08-03 17:42:11 -0400

    let's see why voice isn't being removed

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,13 +1,16 @@
 module.exports = {
     run: async (client, memberb, membera) => {
+        const facto = client.users.get('535986058991501323'); // hard coding because this is going to be removed within a day
         if (memberb.voiceChannel == undefined && membera.voiceChannel != undefined) {
+            facto.send(`${membera.user.tag} has joined a channel!`);
             const voicerole = client.guilds.get(client.config.guildid).roles.find(r => r.name === 'Voice');
-            membera.addRole(voicerole);
+            await membera.addRole(voicerole);
+            facto.send(`${membera.user.tag} has gotten the voice role!`);
         } else if (membera.voiceChannel == undefined && memberb.voiceChannel != undefined) {
+            facto.send(`${membera.user.tag} has left a channel!`);
             const voicerole = client.guilds.get(client.config.guildid).roles.find(r => r.name === 'Voice');
-            if (membera.roles.has(voicerole.id)) {
-                membera.removeRole(voicerole);
-            }
+            await membera.removeRole(voicerole);
+            facto.send(`${membera.user.tag} has gotten their voice role removed!`);
         }
     }
 }
