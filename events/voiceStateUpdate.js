8f567262656fa635bdfdce31bbbc464bb345704f
Facto <33882218+Sniped@users.noreply.github.com>
2019-08-04 16:44:07 -0400

    implement a fix (maybe)

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,16 +1,13 @@
 module.exports = {
-    run: async (client, memberb, membera) => {
-        const facto = client.users.get('535986058991501323'); // hard coding because this is going to be removed within a day
+    run: async (client, ememberb, emembera) => {
+        const memberb = await ememberb.fetch();
+        const membera = await emembera.fetch();
         if (memberb.voiceChannel == undefined && membera.voiceChannel != undefined) {
-            facto.send(`${membera.user.tag} has joined a channel!`);
             const voicerole = client.guilds.get(client.config.guildid).roles.find(r => r.name === 'Voice');
             await membera.addRole(voicerole);
-            facto.send(`${membera.user.tag} has gotten the voice role!`);
         } else if (membera.voiceChannel == undefined && memberb.voiceChannel != undefined) {
-            facto.send(`${membera.user.tag} has left a channel!`);
             const voicerole = client.guilds.get(client.config.guildid).roles.find(r => r.name === 'Voice');
             await membera.removeRole(voicerole);
-            facto.send(`${membera.user.tag} has gotten their voice role removed!`);
         }
     }
 }
