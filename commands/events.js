c04d5c6f17646f52719fbbd06e2df41e2dca6d4a
Facto <33882218+Sniped@users.noreply.github.com>
2019-08-16 20:59:14 -0400

    typo

--- commands/events.js
+++ commands/events.js
@@ -3,7 +3,7 @@ module.exports = {
         const message = args.join(' ');
         if (!message) return msg.channel.send(':x: You must supply a message to announce in the events channel!');
         const events = client.channels.get(client.config.eventschannel);
-        const eventsrole = client.guilds.get(client.config.guilid).roles.find(r => r.name === 'Events');
+        const eventsrole = client.guilds.get(client.config.guildid).roles.find(r => r.name === 'Events');
         await eventsrole.setMentionable(true);
         await events.send(`${message}\n\n<@&${eventsrole.id}>`);
         await eventsrole.setMentionable(false);
