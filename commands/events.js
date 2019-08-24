c3845ef436f284f31411c6d121903e55e446c69e
Facto <33882218+Sniped@users.noreply.github.com>
2019-08-24 17:40:22 -0400

    make tagging events role optional

--- commands/events.js
+++ commands/events.js
@@ -4,9 +4,23 @@ module.exports = {
         if (!message) return msg.channel.send(':x: You must supply a message to announce in the events channel!');
         const events = client.channels.get(client.config.eventschannel);
         const eventsrole = client.guilds.get(client.config.guildid).roles.find(r => r.name === 'Events');
-        await eventsrole.setMentionable(true);
-        await events.send(`${message}\n\n<@&${eventsrole.id}>`);
-        await eventsrole.setMentionable(false);
+        const awaitM = async () => {
+            const filter = m => m.author.id === msg.author.id;
+            const c = await msg.channel.awaitMessages(filter, { max: 1, time: 60000 * 5, errors: ['time'] })
+            return c;
+        }
+        msg.channel.send('Would you like to tag the events role? (y/n)');
+        m = await awaitM();
+        if (m.first().content.toLowerCase() == 'y') {
+            await eventsrole.setMentionable(true);
+            await events.send(`${message}\n\n<@&${eventsrole.id}>`);
+            await eventsrole.setMentionable(false);    
+        } else if (m.first().content.toLowerCase() == 'n') {
+            await events.send(`${message}`);
+        } else {
+            msg.channel.send(':x: Invalid response, defaulting to `n`');
+            await events.send(`${message}`);
+        }
     },
     meta: {
         aliases: ['events'],
