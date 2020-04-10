c897a083a565f7b7ae4d1abfc035c7498906eb8f
theminecoder <theminecoder.dev@gmail.com>
2020-04-10 16:19:47 +1000

    make init command use updated user init object

--- commands/initialize.js
+++ commands/initialize.js
@@ -1,15 +1,20 @@
 module.exports = {
     run: async (client, msg, args) => {
         const members = (await msg.guild.fetchMembers()).members;
+        const muted = await msg.guild.roles.find(r => r.name === 'Muted');
         members.forEach(async member => {
             const user = await client.db.table('userData').get(member.id).run();
             if (!user) {
-                const muted = msg.guild.roles.find(r => r.name === 'Muted');
-                if (member.roles.has(muted.id)) {
-                    client.db.table('userData').insert({ id: member.id, muted: true, msgs: 0 }).run();
-                } else {
-                    client.db.table('userData').insert({ id: member.id, muted: false, msgs: 0 }).run();
-                }
+                client.db.table('userData').insert({id: member.id,
+                    user: {
+                        id: member.id,
+                        tag: member.user.tag,
+                        username: member.user.username,
+                        avatarURL: member.user.avatarURL
+                    },
+                    muted: member.roles.has(muted.id),
+                    msgs: 0
+                }).run();
             }
         });
         msg.channel.send(':ok_hand: done');
@@ -19,6 +24,6 @@ module.exports = {
         description: '',
         permlvl: 6,
         modCmd: false,
-        usage: ''              
+        usage: ''
     }
 }
