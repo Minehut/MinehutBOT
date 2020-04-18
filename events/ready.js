0cbf1d258fbbd3a992be1dc753495b92659a9ca7
Daniel Gulic <danielgulic@gmail.com>
2020-04-19 01:34:42 +1000

    delete things for rewrite

--- events/ready.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    run: async (client) => {
-        console.log(`Ready as ${client.user.tag}`);
-        client.user.setActivity(`with ${client.guilds.get(client.config.guildid).memberCount} users`, { type: 'PLAYING' });    }
-}
