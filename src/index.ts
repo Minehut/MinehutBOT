7f10fbbf588c3f03632db06a9b0b0a4ca4699b5e
Daniel Gulic <danielgulic@gmail.com>
2020-04-19 04:26:51 +1000

    init base structure + ping command

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import MinehutClient from './client/MinehutClient';
+
+require('dotenv').config();
+
+// TODO: validate env variables
+const client = new MinehutClient({
+  ownerIds: process.env.OWNER_IDS?.split(' '),
+  prefix: process.env.DISCORD_PREFIX || '!'
+});
+client.login(process.env.DISCORD_TOKEN);
