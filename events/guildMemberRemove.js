e648aef6a78156b03bd9bf51dd1bb8510cfd3946
Daniel Gulic <danielgulic@gmail.com>
2020-04-19 01:34:42 +1000

    delete things for rewrite

--- events/guildMemberRemove.js
+++
@@ -1,9 +0,0 @@
-module.exports = { 
-    run: async (client, member) => {
-        if (member.guild.id === client.config.staffguild) {
-            client.log(`\`[STAFF]\` :outbox_tray: ${member.user.tag} (\`${member.id}\`) left the server`);
-        } else {
-            client.log(`:outbox_tray: ${member.user.tag} (\`${member.id}\`) left the server`);
-        }
-    }
-}
--- events/userUpdate.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    run: async (client, userb, usera) => {
-        if (userb.tag != usera.tag) {
-            client.log(`:name_badge: ${usera.tag} (\`${usera.id}\`) changed username from \`${userb.tag}\` to \`${usera.tag}\``);
-        } else return;
-    }
-}
