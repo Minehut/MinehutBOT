f6ffe99ad127675a80b9c65ed6da454df527b279
Daniel Gulic <danielgulic@gmail.com>
2020-04-22 02:02:20 +1000

    add constant messages for mylevel cmd

--- src/util/constants/messages.ts
+++ src/util/constants/messages.ts
@@ -4,9 +4,12 @@ export const messages = {
 			description: 'Ping, pong',
 			responseLoading: ':ping_pong: Ping?',
 			responseFinished: (roundtrip: Number, wsPing: Number) =>
-				`:ping_pong: Pong! (Roundtrip: ${
-					roundtrip
-				}ms | One-way: ${wsPing}ms)`,
+				`:ping_pong: Pong! (Roundtrip: ${roundtrip}ms | One-way: ${wsPing}ms)`,
+		},
+		myLevel: {
+			description: 'Show your permission level',
+			response: (permLevel: Number, permLevelName: string) =>
+				`Your permission level is ${permLevel} (${permLevelName})`,
 		},
 	},
 };
