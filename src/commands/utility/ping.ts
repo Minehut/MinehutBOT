6b39bf2bc1442d965cd87212c18fed51f97162ad
Daniel Gulic <danielgulic@gmail.com>
2020-04-21 22:10:50 +1000

    rename ping command file (camelCase) and use constant messages, description object

---
+++ src/commands/utility/ping.ts
@@ -0,0 +1,25 @@
+import { Command } from 'discord-akairo';
+import { Message } from 'discord.js';
+import { messages } from '../../messages';
+
+export default class PingCommand extends Command {
+	constructor() {
+		super('ping', {
+      aliases: ['ping'],
+      category: 'utility',
+      description: {
+        content: messages.commands.ping.description
+      }
+		});
+	}
+
+	async exec(msg: Message) {
+		const m = await msg.channel.send(messages.commands.ping.responseLoading);
+		m.edit(
+			messages.commands.ping.responseFinished(
+				m.createdTimestamp - msg.createdTimestamp,
+				~~this.client.ws.ping
+			)
+		);
+	}
+}
