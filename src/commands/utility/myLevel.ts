1aa8f09225ef5d206302a8bee8394891399faeb0
Daniel Gulic <danielgulic@gmail.com>
2020-04-22 02:01:43 +1000

    rename myLevel file (camelCase), use constant messages

--- src/commands/utility/myLevel.ts
+++ src/commands/utility/myLevel.ts
@@ -2,17 +2,23 @@ import { Command } from 'discord-akairo';
 import { Message } from 'discord.js';
 import getPermissionLevel from '../../util/permission/getPermissionLevel';
 import { PermissionLevel } from '../../util/permission/PermissionLevel';
+import { messages } from '../../util/constants/messages';
 
 export default class PingCommand extends Command {
 	constructor() {
 		super('mylevel', {
-      aliases: ['mylevel'],
-      channel: 'guild'
+			aliases: ['mylevel'],
+			channel: 'guild',
+			description: {
+				content: messages.commands.myLevel.description,
+			},
 		});
 	}
 
 	async exec(msg: Message) {
-    const permLevel = getPermissionLevel(msg.member!, this.client);
-		msg.channel.send(`Your permission level is ${permLevel} (${PermissionLevel[permLevel]})`);
+		const permLevel = getPermissionLevel(msg.member!, this.client);
+		msg.channel.send(
+			messages.commands.myLevel.response(permLevel, PermissionLevel[permLevel])
+		);
 	}
 }
