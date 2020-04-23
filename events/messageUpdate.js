7705d2e3af9d360f836319416d684aef7d30ef33
Daniel Gulic <danielgulic@gmail.com>
2020-04-23 13:49:43 +1000

    add chat filter

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -1,13 +1,31 @@
+const filters = require('../filter.json');
 module.exports = {
     run: async (client, msgb, msga) => {
         if (msgb.embeds.length == 0 && msgb.content != msga.content && msga.channel.id && !msga.author.bot) {
+					if (msga.guild.id === client.config.guildid && (msga.channel.id !== '585753135742582786') && msga.channel.id !== '676533595648688149') {
+						// Chat filter
+						const matches = filters.filter(f => f.enabled && new RegExp(f.rule).test(msga.content.toLowerCase()));
+						if (matches.length > 0) {
+							const match = matches[0];
+							msga.delete();
+							switch (match.type.toLowerCase()) {
+								case 'swear':
+									msga.channel.send(`${msga.author}, please do not swear on the Minehut discord. Thanks! ^_^`);
+									break;
+								case 'spam':
+									msga.channel.send(`${msga.author}, please do not spam on the Minehut discord. Thanks! ^_^`);
+									break;
+							}
+							return;
+						}
+					}
             if (msgb.guild.id === client.config.staffguild) {
                 const logmsg = await client.log(`\`[STAFF]\` :pencil: ${msgb.author.tag} (\`${msgb.author.id}\`) message edited in **#${msgb.channel.name}**: \n**B**: ${client.replaceMentions(msgb)}\n**A**: ${await client.replaceMentions(msga)}`);
                 client.sendAttachments(msga, logmsg);    
             } else {
                 const logmsg = await client.log(`:pencil: ${msgb.author.tag} (\`${msgb.author.id}\`) message edited in **#${msgb.channel.name}**: \n**B**: ${client.replaceMentions(msgb)}\n**A**: ${await client.replaceMentions(msga)}`);
                 client.sendAttachments(msga, logmsg);    
-            }
+						}
             if (msga.content.includes('discord.gg')) {
                 const msgarray = msga.content.split(" ");    
                 const invitestring = msgarray.filter(element => {
