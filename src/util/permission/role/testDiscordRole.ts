26b512cb3ddd72b91d21a4396ef15c36af93e768
Daniel Gulic <danielgulic@gmail.com>
2020-05-01 21:35:37 +1000

    Add support for test discord roles (so I don't need to replace main to test)

---
+++ src/util/permission/role/testDiscordRole.ts
@@ -0,0 +1,21 @@
+export enum TestDiscordRole {
+	Muted = '',
+
+	DJ = '',
+	Verified = '',
+	BoostersPass = '',
+	NitroBooster = '',
+
+	Advisor = '',
+	SuperLeague = '',
+	YouTube = '',
+	BuildTeam = '',
+	Robot = '',
+
+	JuniorModerator = '701854457331187763',
+	Moderator = '',
+	SeniorModerator = '',
+	Manager = '',
+	Developer = '',
+	Admin = '701854509164527707',
+}
