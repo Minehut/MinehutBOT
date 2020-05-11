9cc337f0235095cea2a780ed214b1ef25613b052
Daniel Gulic <danielgulic@gmail.com>
2020-05-12 01:03:22 +1000

    refactor permission levels, add guild configs

--- src/util/permission/getPermissionLevel.ts
+++ src/util/permission/getPermissionLevel.ts
@@ -1,96 +1,84 @@
 import { GuildMember } from 'discord.js';
 import { MinehutClient } from '../../client/minehutClient';
-import { MainDiscordRole } from './role/mainDiscordRole';
 import { PermissionLevel } from './permissionLevel';
-import { StaffDiscordRole } from './role/staffDiscordRole';
-import { TestDiscordRole } from './role/testDiscordRole';
+import { guildConfigs } from '../config/guild/guildConfigs';
+import { RoleType } from './roleType';
+
+const getRoleTypeById = (id: string): string | null => {
+	let roleType: string | null = null;
+	guildConfigs.forEach(g =>
+		Object.keys(g.roles).forEach(k => {
+			if (g.roles[k] === id) roleType = k;
+		})
+	);
+	return roleType;
+};
 
 export function getPermissionLevel(member: GuildMember, client: MinehutClient) {
-	if (client.ownerIds?.includes(member.id)) return PermissionLevel.BotDeveloper;
-	const roles = member.roles.cache.filter(
-		r =>
-			Object.values(MainDiscordRole).some(v => v === r.id) ||
-			Object.values(StaffDiscordRole).some(v => v === r.id) ||
-			Object.values(TestDiscordRole).some(v => v === r.id)
+	// They have BotDeveloper perm level if they are in the bot owner list
+	if (client.ownerIds?.includes(member.id)) {} //return PermissionLevel.BotDeveloper;
+	// We want to get a list of every significant role id from every guild
+	let allRoles: string[] = [];
+	guildConfigs.forEach(
+		g => (allRoles = allRoles.concat(Object.values(g.roles) as string[]))
 	);
+	// We only want to look at roles that we know about from the member
+	const roles = member.roles.cache.filter(memberRole =>
+		allRoles.includes(memberRole.id)
+	);
+	// If they don't have any significant roles, they have Everyone perm level
 	if (roles.size < 1) return PermissionLevel.Everyone;
-	const highestRole = roles.sort((a, b) => a.position - b.position).last();
 
-	switch (highestRole!.id) {
-		case TestDiscordRole.Muted:
-		case MainDiscordRole.Muted:
+	const highestRole = roles.sort((a, b) => a.position - b.position).last();
+	const roleType = getRoleTypeById(highestRole?.id!);
+	if (!roleType) throw 'Something weird happened, roleType is not defined';
+
+	switch (roleType) {
+		case RoleType.Muted:
 			return PermissionLevel.Muted;
 
-		case TestDiscordRole.DJ:
-		case MainDiscordRole.DJ:
+		case RoleType.DJ:
 			return PermissionLevel.DJ;
-
-		case TestDiscordRole.Verified:
-		case MainDiscordRole.Verified:
+		
+		case RoleType.Verified:
 			return PermissionLevel.Verified;
-
-		case TestDiscordRole.BoostersPass:
-		case MainDiscordRole.BoostersPass:
+		
+		case RoleType.BoostersPass:
 			return PermissionLevel.BoostersPass;
-
-		case TestDiscordRole.NitroBooster:
-		case StaffDiscordRole.NitroBooster:
-		case MainDiscordRole.NitroBooster:
+		
+		case RoleType.NitroBooster:
 			return PermissionLevel.NitroBooster;
 
-		case TestDiscordRole.Advisor:
-		case StaffDiscordRole.Advisor:
-		case MainDiscordRole.Advisor:
+		case RoleType.Advisor:
 			return PermissionLevel.Advisor;
-
-		case TestDiscordRole.SuperLeague:
-		case StaffDiscordRole.SuperLeague:
-		case MainDiscordRole.SuperLeague:
+		
+		case RoleType.SuperLeague:
 			return PermissionLevel.SuperLeague;
-
-		case TestDiscordRole.YouTube:
-		case MainDiscordRole.YouTube:
+		
+		case RoleType.YouTube:
 			return PermissionLevel.YouTube;
-
-		case TestDiscordRole.YouTube:
-		case StaffDiscordRole.Builder:
-		case MainDiscordRole.BuildTeam:
-			return PermissionLevel.BuildTeam;
-
-		case TestDiscordRole.Robot:
-		case StaffDiscordRole.Robot:
-		case MainDiscordRole.Robot:
+		
+		case RoleType.Robot:
 			return PermissionLevel.Robot;
 
-		case TestDiscordRole.JuniorModerator:
-		case StaffDiscordRole.JuniorModerator:
-		case MainDiscordRole.JuniorModerator:
+		case RoleType.JuniorModerator:
 			return PermissionLevel.JuniorModerator;
-
-		case TestDiscordRole.Moderator:
-		case StaffDiscordRole.Moderator:
-		case MainDiscordRole.Moderator:
+		
+		case RoleType.Moderator:
 			return PermissionLevel.Moderator;
 
-		case TestDiscordRole.SeniorModerator:
-		case StaffDiscordRole.SeniorModerator:
-		case MainDiscordRole.SeniorModerator:
+		case RoleType.SeniorModerator:
 			return PermissionLevel.SeniorModerator;
 
-		case TestDiscordRole.Manager:
-		case StaffDiscordRole.Manager:
-		case MainDiscordRole.Manager:
+		case RoleType.Manager:
 			return PermissionLevel.Manager;
 
-		case TestDiscordRole.Developer:
-		case StaffDiscordRole.Developer:
+		case RoleType.Developer:
 			return PermissionLevel.Developer;
 
-		case TestDiscordRole.Admin:
-		case StaffDiscordRole.Admin:
-		case MainDiscordRole.Admin:
+		case RoleType.Admin:
 			return PermissionLevel.Admin;
-
+		
 		default:
 			return PermissionLevel.Everyone;
 	}
