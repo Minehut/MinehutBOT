9c5b354b7d8cc6d92eb9e2230e9c50dcd83f5fbe
Daniel Gulic <danielgulic@gmail.com>
2020-05-12 01:03:22 +1000

    refactor permission levels, add guild configs

---
+++ src/util/config/guild/guildConfiguration.ts
@@ -0,0 +1,7 @@
+import { RoleList } from "./roleList";
+
+// The GuildConfiguration can have settings about permissions, log channels, disabled features, etc.
+export interface GuildConfiguration {
+	id: string;
+	roles: RoleList;
+}
---
+++ src/util/permission/roleType.ts
@@ -0,0 +1,24 @@
+export enum RoleType {
+	Muted = 'muted',
+
+	DJ = 'dj',
+	Verified = 'verified',
+	BoostersPass = 'boostersPass',
+	NitroBooster = 'nitroBooster',
+
+	Advisor = 'advisor',
+	SuperLeague = 'superLeague',
+	YouTube = 'youTube',
+	Robot = 'robot',
+	BuildTeam = 'buildTeam',
+	Builder = 'builder',
+
+	Staff = 'staff',
+	Mentor = 'mentor',
+	JuniorModerator = 'juniorModerator',
+	Moderator = 'moderator',
+	SeniorModerator = 'seniorModerator',
+	Manager = 'manager',
+	Developer = 'developer',
+	Admin = 'admin'
+}
