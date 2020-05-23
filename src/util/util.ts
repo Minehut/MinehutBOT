fafc8a102497891a7c5f37c2f238318e6f2fc88d
Daniel Gulic <danielgulic@gmail.com>
2020-05-23 20:31:51 +1000

    humanReadableCaseType util function

---
+++ src/util/util.ts
@@ -0,0 +1,48 @@
+import { CaseType } from './constants';
+
+export function humanReadableCaseType(
+	type: CaseType,
+	startUppercase: boolean = true
+) {
+	let readable;
+	switch (type) {
+		case CaseType.Ban:
+			readable = 'banned';
+			break;
+		case CaseType.ForceBan:
+			readable = 'forcebanned';
+			break;
+		case CaseType.Kick:
+			readable = 'kicked';
+			break;
+		case CaseType.Mute:
+			readable = 'muted';
+			break;
+		case CaseType.SoftBan:
+			readable = 'softbanned';
+			break;
+		case CaseType.UnMute:
+			readable = 'unmuted';
+			break;
+		case CaseType.UnVoiceMute:
+			readable = 'unvoicemuted';
+			break;
+		case CaseType.Unban:
+			readable = 'unbanned';
+			break;
+		case CaseType.VoiceKick:
+			readable = 'voicekicked';
+			break;
+		case CaseType.VoiceMute:
+			readable = 'voicemuted';
+			break;
+		case CaseType.Warn:
+			readable = 'warned';
+			break;
+		default:
+			readable = 'punished';
+	}
+	return startUppercase
+		? readable.charAt(0).toUpperCase() + readable.slice(1)
+		: readable;
+}
