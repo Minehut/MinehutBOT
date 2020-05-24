e3dba2991b3a39bde82462d798ec54a6c1004457
Daniel Gulic <danielgulic@gmail.com>
2020-05-24 21:55:08 +1000

    Make sure the bot doesn't try to message itself

--- src/structure/action/warnAction.ts
+++ src/structure/action/warnAction.ts
@@ -59,6 +59,7 @@ export class WarnAction {
 	}
 
 	async sendTargetDm() {
+		if (this.target.id === this.message.client.user?.id) return; // The bot can't message itself
 		const embed = new MessageEmbed()
 			.setColor('RED')
 			.setDescription(
