10c9d9a3b2e15647f3b55bb18f4610203200b75f
Daniel Gulic <danielgulic@gmail.com>
2020-06-14 22:33:16 +1000

    Escape markdown function

--- src/util/util.ts
+++ src/util/util.ts
@@ -74,3 +74,26 @@ export function prettyDate(
 				relative ? ` (${ago.format(date)})` : ''
 		  }`;
 }
+
+const replacements = [
+	[/\*/g, '\\*', 'asterisks'],
+	[/#/g, '\\#', 'number signs'],
+	[/\//g, '\\/', 'slashes'],
+	[/\(/g, '\\(', 'parentheses'],
+	[/\)/g, '\\)', 'parentheses'],
+	[/\[/g, '\\[', 'square brackets'],
+	[/\]/g, '\\]', 'square brackets'],
+	[/</g, '&lt;', 'angle brackets'],
+	[/>/g, '&gt;', 'angle brackets'],
+	[/_/g, '\\_', 'underscores'],
+	[/~~/g, '\\~\\~', 'tildas'],
+];
+
+export function escapeMarkdown(content: string, skips: string[] = []) {
+	return replacements.reduce((string, replacement) => {
+		const name = replacement[2] as string;
+		return name && skips.indexOf(name) !== -1
+			? string
+			: string.replace(replacement[0], replacement[1] as string);
+	}, content);
+}
