953baeacc77d61aad867791a03f2cd7aed482c94
Daniel Gulic <danielgulic@gmail.com>
2020-07-07 18:29:42 +1000

    Clean command

---
+++ src/command/mod/clean/clean.ts
@@ -0,0 +1,42 @@
+import { Flag } from 'discord-akairo';
+import { Message } from 'discord.js';
+import { messages } from '../../../util/messages';
+import { PrefixSupplier } from 'discord-akairo';
+import { MinehutCommand } from '../../../structure/command/minehutCommand';
+import { PermissionLevel } from '../../../util/permission/permissionLevel';
+
+export default class CleanCommand extends MinehutCommand {
+	constructor() {
+		super('clean', {
+			aliases: ['clean', 'prune'],
+			description: {
+				content: `Clean messages
+				Available subcommands:
+				• **user** \`<user> <count>\`
+				• **all** \`<count>\`
+				• **bots** \`<count>\`
+				`,
+				usage: '<method> <...arguments>',
+			},
+			permissionLevel: PermissionLevel.JuniorModerator,
+			category: 'mod',
+			channel: 'guild',
+		});
+	}
+
+	*args() {
+		const method = yield {
+			type: [
+				['clean-user', 'user'],
+				['clean-all', 'all'],
+				['clean-bots', 'bots'],
+			],
+			otherwise: (msg: Message) => {
+				const prefix = (this.handler.prefix as PrefixSupplier)(msg) as string;
+				return messages.commands.common.useHelp(prefix, this.aliases[0]);
+			},
+		};
+
+		return Flag.continue(method);
+	}
+}
