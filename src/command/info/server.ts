0c6c5e58d94d00dd25b2d1ea84ad422a86e4230a
Daniel Gulic <danielgulic@gmail.com>
2020-07-15 02:13:57 +1000

    Last Online -> Last Started

--- src/command/info/server.ts
+++ src/command/info/server.ts
@@ -44,7 +44,7 @@ export default class ServerInfoCommand extends MinehutCommand {
 			);
 			embed.setColor(server.online ? 'GREEN' : 'RED');
 			embed.setDescription(server.motd.replace(COLOUR_CODE_EXPR, ''));
-			embed.addField('Last Online', prettyDate(server.lastOnline), true);
+			embed.addField('Last Started', prettyDate(server.lastOnline), true);
 			if (server.playerCount > 0)
 				embed.addField('Player Count', server.playerCount, true);
 			embed.addField('Suspended?', server.suspended ? 'Yes' : 'No', true);
