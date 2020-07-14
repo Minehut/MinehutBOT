5773ba35cce0988d952bd5dff636b235bbba7172
Daniel Gulic <danielgulic@gmail.com>
2020-07-14 20:43:20 +1000

    Only log in one guild (nickname update, roles update)

--- src/listener/modLog/memberNickNameUpdate.ts
+++ src/listener/modLog/memberNickNameUpdate.ts
@@ -12,31 +12,30 @@ export default class ModLogMemberNickNameUpdateListener extends Listener {
 	}
 
 	async exec(oldMember: GuildMember, newMember: GuildMember) {
-		guildConfigs.forEach(async config => {
-			if (
-				!config ||
-				!config.features.modLog ||
-				!config.features.modLog.events.includes('memberNickNameUpdate') ||
-				oldMember.nickname === newMember.nickname
-			)
-				return;
-			const guild = this.client.guilds.cache.get(config.id);
-			if (!guild) return;
-			if (oldMember.nickname && !newMember.nickname)
-				return await sendModLogMessage(
-					guild,
-					`:name_badge: ${oldMember.user.tag} (\`${oldMember.id}\`) removed nick \`${oldMember.nickname}\``
-				);
-			else if (!oldMember.nickname && newMember.nickname)
-				return await sendModLogMessage(
-					guild,
-					`:name_badge: ${oldMember.user.tag} (\`${oldMember.id}\`) added nick \`${newMember.nickname}\``
-				);
-			else if (oldMember.nickname !== newMember.nickname)
-				return await sendModLogMessage(
-					guild,
-					`:name_badge: ${oldMember.user.tag} (\`${oldMember.id}\`) changed nick to \`${newMember.nickname}\` from \`${oldMember.nickname}\``
-				);
-		});
+		const config = guildConfigs.get(oldMember.guild.id);
+		if (
+			!config ||
+			!config.features.modLog ||
+			!config.features.modLog.events.includes('memberNickNameUpdate') ||
+			oldMember.nickname === newMember.nickname
+		)
+			return;
+		const guild = this.client.guilds.cache.get(config.id);
+		if (!guild) return;
+		if (oldMember.nickname && !newMember.nickname)
+			return await sendModLogMessage(
+				guild,
+				`:name_badge: ${oldMember.user.tag} (\`${oldMember.id}\`) removed nick \`${oldMember.nickname}\``
+			);
+		else if (!oldMember.nickname && newMember.nickname)
+			return await sendModLogMessage(
+				guild,
+				`:name_badge: ${oldMember.user.tag} (\`${oldMember.id}\`) added nick \`${newMember.nickname}\``
+			);
+		else if (oldMember.nickname !== newMember.nickname)
+			return await sendModLogMessage(
+				guild,
+				`:name_badge: ${oldMember.user.tag} (\`${oldMember.id}\`) changed nick to \`${newMember.nickname}\` from \`${oldMember.nickname}\``
+			);
 	}
 }
