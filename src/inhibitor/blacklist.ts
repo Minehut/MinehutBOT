35ea1101ab659e6c223e3bb5b245d6851da6fdb3
Daniel Gulic <danielgulic@gmail.com>
2020-09-12 01:37:29 +1000

    Format blacklist file

--- src/inhibitor/blacklist.ts
+++ src/inhibitor/blacklist.ts
@@ -1,22 +1,20 @@
-import {Message} from "discord.js";
+import { Message } from 'discord.js';
 
 const { Inhibitor } = require('discord-akairo');
 
 class BlacklistInhibitor extends Inhibitor {
+	//TODO Store in mongo
+	private blacklist: Array<string> = [];
 
-    //TODO Store in mongo
-    // noinspection JSMismatchedCollectionQueryUpdate
-    private blacklist: Array<string> = [];
+	constructor() {
+		super('blacklist', {
+			reason: 'blacklist',
+		});
+	}
 
-    constructor() {
-        super('blacklist', {
-            reason: 'blacklist'
-        })
-    }
-
-    exec(message: Message) {
-        return this.blacklist.includes(message.author.id);
-    }
+	exec(message: Message) {
+		return this.blacklist.includes(message.author.id);
+	}
 }
 
 module.exports = BlacklistInhibitor;
