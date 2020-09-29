3de1733bffd16b510ed4a9339c877ebad25f6fd3
Sniped <facto@facto.club>
2020-09-28 21:31:48 -0400

    add comments from copied file

--- src/util/permission/hasPermissionLevelRole.ts
+++ src/util/permission/hasPermissionLevelRole.ts
@@ -5,10 +5,12 @@ import { PermissionLevel } from "./permissionLevel";
 import { resolvePermissionLevelFromRoleType } from "./resolvePermissionLevelFromRoleType";
 
 export function hasPermissionLevelRole(permissionLevel: PermissionLevel, member: GuildMember) {
+	// We want to get a list of every significant role id from every guild
     let allRoles: string[] = [];
 	guildConfigs.forEach(
 		g => (allRoles = allRoles.concat(Object.values(g.roles) as string[]))
 	);
+	// We only want to look at roles that we know about from the member
     const roles = member.roles.cache.filter(memberRole =>
 		allRoles.includes(memberRole.id)
     );
