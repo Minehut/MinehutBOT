77a89f5adaf37dd531d921524a5d963f7ad78d8c
Sniped <facto@facto.club>
2020-09-28 21:28:33 -0400

    no more default imports

--- src/guild/config/guildConfiguration.ts
+++ src/guild/config/guildConfiguration.ts
@@ -4,7 +4,7 @@ import { ReactionRoleConfiguration } from './feature/reactionRole';
 import { AnnouncementConfiguration } from './feature/announcement';
 import { CensorConfiguration } from './feature/censor';
 import { AutoReactConfiguration } from './feature/autoReact';
-import BoosterPassConfiguration from './feature/boosterPass';
+import { BoosterPassConfiguration } from './feature/boosterPass';
 
 // The GuildConfiguration can have settings about permissions, log channels, disabled features, etc.
 export interface GuildConfiguration {
