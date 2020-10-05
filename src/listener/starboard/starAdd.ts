e17054840c6b1802fbb2018702513fd570e3d399
Spiderlogical <thehelpfulspider@gmail.com>
2020-10-04 22:33:05 -0700

    Update star listener, moved sending msg to class

--- src/listener/starboard/starAdd.ts
+++ src/listener/starboard/starAdd.ts
@@ -2,11 +2,10 @@ import { Listener } from 'discord-akairo';
 import { guildConfigs } from '../../guild/config/guildConfigs';
 import { MessageReaction } from 'discord.js';
 import { TextChannel } from 'discord.js';
-import { MessageEmbed } from 'discord.js';
-import { IMGUR_LINK_REGEX } from '../../util/constants';
 import { User } from 'discord.js';
 import { getPermissionLevel } from '../../util/permission/getPermissionLevel';
 import { PermissionLevel } from '../../util/permission/permissionLevel';
+import { Star } from '../../structure/starboard/star';
 
 export default class StarAddListener extends Listener {
 	constructor() {
@@ -17,52 +16,47 @@ export default class StarAddListener extends Listener {
 	}
 
 	async exec(reaction: MessageReaction, user: User) {
-		const message = reaction.message;
-		if (!message.guild) return;
-		const config = guildConfigs.get(message.guild.id);
+		const msg = reaction.message;
+		if (!msg.guild) return;
+		const config = guildConfigs.get(msg.guild.id);
+		
 		if (
 			!config ||
 			!config.features.starboard ||
 			!config.features.starboard.channel ||
 			!config.features.starboard.triggerAmount ||
-			(config.features.starboard.ignoredChannels && config.features.starboard.ignoredChannels.includes(message.channel.id))
+			(config.features.starboard.ignoredChannels && config.features.starboard.ignoredChannels.includes(msg.channel.id))
 		)
 			return;
 		
-		let member = message.guild.member(user)
-		if (!member) return;
-		if (getPermissionLevel(member, this.client) === PermissionLevel.Muted) return reaction.remove();
+		const exists = await Star.exists(msg.id)
+		if (exists) return;
 
-		let triggerAmount = config.features.starboard.triggerAmount;
-		let count = reaction.count;
+		const minLevel = config.features.starboard.minimumPermLevel || PermissionLevel.Everyone
+		const triggerEmoji = config.features.starboard.emoji || "⭐"
+
+		const member = msg.guild.member(user)!;
+		if (getPermissionLevel(member, this.client) === PermissionLevel.Muted) return reaction.remove();
+		
+		//if (msg.author.id === user.id && reaction.emoji.toString() === triggerEmoji) return reaction.remove()
+
+		const triggerAmount = config.features.starboard.triggerAmount;
+		const count = reaction.count;
 		if (!count) return;
 		
-		const emoji = reaction.emoji;
-		if (emoji.toString() === "⭐") {
-			if (count >= triggerAmount) {
-				const channel = message.guild.channels.cache.get(config.features.starboard.channel) as TextChannel
-				const member = message.guild.member(message.author)
-				const embed = new MessageEmbed()
-					.setColor('YELLOW')
-					.setTimestamp()
-					.setAuthor(member?.displayName, message.author.displayAvatarURL());
-				
-				let matches = message.content.match(IMGUR_LINK_REGEX);
-				let content = message.content;
-				if (matches) {
-					const link = matches[0];
-					content = message.content.replace(link, '');
-					embed.setImage(link);
-				}
-				else if (message.attachments) {
-					let firstAttached = message.attachments.first()
-					if (firstAttached?.width) {
-						embed.setImage(firstAttached.url)
-					}
-				}
-				embed.setDescription(`${content}\n\n[Jump!](${message.url})`)
-				
-				channel.send(`⭐**${count}** ${message.channel} `, embed)
+		const canStar = reaction.users.cache.some(user => getPermissionLevel(msg.guild?.member(user)!, this.client) >= minLevel)
+
+		const addedEmoji = reaction.emoji;
+		console.log(addedEmoji.toString())
+		if (Star.emojiEquals(addedEmoji, triggerEmoji) || Star.emojiEquals(addedEmoji.toString(), triggerEmoji)) {
+			if (count >= triggerAmount && canStar) {
+				const channel = msg.guild.channels.cache.get(config.features.starboard.channel) as TextChannel;
+				const star: Star = new Star({
+					msg,
+					channel,
+					count
+				});
+				star.commit();
 			}
 			
 		}
