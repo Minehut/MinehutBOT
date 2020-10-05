704494edddfef2d99dbff9c7e472826edfac2df6
Spiderlogical <thehelpfulspider@gmail.com>
2020-10-04 22:32:19 -0700

    Add emoji option to config

--- src/guild/config/feature/starboard.ts
+++ src/guild/config/feature/starboard.ts
@@ -3,6 +3,7 @@ import { PermissionLevel } from "../../../util/permission/permissionLevel";
 export interface StarboardConfiguration {
     channel: string;
     triggerAmount: number;
+    emoji?: string;
     minimumPermLevel?: PermissionLevel;
     ignoredChannels?: string[];
 }
