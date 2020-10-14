c1d6035225e0099592c83ef1c444fe33700a819e
Spiderlogical <thehelpfulspider@gmail.com>
2020-10-13 23:37:44 -0700

    prettify and minor changes

--- src/listener/starboard/starAdd.ts
+++ src/listener/starboard/starAdd.ts
@@ -21,76 +21,78 @@ export default class StarAddListener extends Listener {
 		const msg = reaction.message;
 		if (!msg.guild) return;
 		const config = guildConfigs.get(msg.guild.id);
-		
+
 		if (
 			!config ||
 			!config.features.starboard ||
 			!config.features.starboard.channel ||
 			!config.features.starboard.triggerAmount ||
-			(config.features.starboard.ignoredChannels && config.features.starboard.ignoredChannels.includes(msg.channel.id))
+			(config.features.starboard.ignoredChannels &&
+				config.features.starboard.ignoredChannels.includes(msg.channel.id))
 		)
 			return;
-		
+
 		if (msg.author.id === this.client.user?.id) return;
-		
-		const minLevel = config.features.starboard.minimumPermLevel || PermissionLevel.Everyone
+
+		const minLevel =
+			config.features.starboard.minimumPermLevel || PermissionLevel.Everyone;
 		const addedEmoji = reaction.emoji;
-		
-		let triggerEmoji;
-		if (config.features.starboard.emoji) {
-			triggerEmoji = Starboard.getEmojiFromId(this.client, config.features.starboard.emoji)
-		}
-		else {
-			triggerEmoji = "⭐"
-		}
+
+		let triggerEmoji = config.features.starboard.emoji
+			? Starboard.getEmojiFromId(this.client, config.features.starboard.emoji)
+			: '⭐';
+
 		if (!Starboard.emojiEquals(addedEmoji, triggerEmoji)) return;
-		
+
 		const member = msg.guild.member(user)!;
-		if (getPermissionLevel(member, this.client) === PermissionLevel.Muted) return reaction.remove();
-		
+		if (getPermissionLevel(member, this.client) === PermissionLevel.Muted)
+			return reaction.remove();
+
 		//if (msg.author.id === user.id && Star.emojiEquals(addedEmoji, triggerEmoji)) return reaction.remove()
 
 		const triggerAmount = config.features.starboard.triggerAmount;
 		const count = reaction.count;
 		if (!count) return;
 
-		const channel = msg.guild.channels.cache.get(config.features.starboard.channel) as TextChannel;
+		const channel = msg.guild.channels.cache.get(
+			config.features.starboard.channel
+		) as TextChannel;
 
-		const exists = await Starboard.exists(msg.id)
+		const exists = await Starboard.exists(msg.id);
 		if (exists) {
-			let entry = await StarModel.findOne({_id: msg.id})
+			let entry = await StarModel.findOne({ _id: msg.id });
 			if (entry?.starredBy.includes(user.id)) return;
 			await entry?.updateOne({
 				starredBy: entry.starredBy.concat(user.id),
-				starAmount: count
-			})
+				starAmount: count,
+			});
 			let author = msg.guild.member(msg);
 			const embed = new MessageEmbed()
-			.setColor('YELLOW')
-			.setTimestamp()
-			.setAuthor(author?.displayName, msg.author.displayAvatarURL())
-			.setDescription(`${msg.content}\n\n[Jump!](${msg.url})`);
-			
-			const img = Starboard.findImg(msg)
+				.setColor('YELLOW')
+				.setTimestamp()
+				.setAuthor(author?.displayName, msg.author.displayAvatarURL())
+				.setDescription(`${msg.content}\n\n[Jump!](${msg.url})`);
+
+			const img = Starboard.findImg(msg);
 			if (img) embed.setImage(img);
-			
+
 			let pinMsg = await channel.messages.fetch(entry!.storageMsg);
-			//console.log(pinMsg)
-			console.log(pinMsg.editable)
-			
+
 			return pinMsg.edit(`⭐**${count}** ${msg.channel} `, embed);
-			return;
 		}
-		
-		const canStar = reaction.users.cache.some(user => getPermissionLevel(msg.guild?.member(user)!, this.client) >= minLevel)
-	
+
+		const canStar = reaction.users.cache.some(
+			user =>
+				getPermissionLevel(msg.guild?.member(user)!, this.client) >= minLevel
+		);
+
 		if (count >= triggerAmount && canStar) {
 			const starredBy = reaction.users.cache.keyArray();
 			const star: Starboard = new Starboard({
 				msg,
 				channel,
 				count,
-				starredBy
+				starredBy,
 			});
 			star.add();
 		}
--- src/structure/starboard/star.ts
+++ src/structure/starboard/star.ts
@@ -1,10 +1,10 @@
-import { TextChannel } from "discord.js";
-import { MessageEmbed } from "discord.js";
-import { Message } from "discord.js";
-import path from "path";
-import { MinehutClient } from "../../client/minehutClient";
-import { StarModel } from "../../model/star";
-import { IMAGE_LINK_REGEX } from "../../util/constants";
+import { TextChannel } from 'discord.js';
+import { MessageEmbed } from 'discord.js';
+import { Message } from 'discord.js';
+import path from 'path';
+import { MinehutClient } from '../../client/minehutClient';
+import { StarModel } from '../../model/star';
+import { IMAGE_LINK_REGEX } from '../../util/constants';
 
 export interface StarboardData {
 	msg: Message;
@@ -24,37 +24,38 @@ export class Starboard {
 		this.msg = data.msg;
 		this.count = data.count;
 		this.starredBy = data.starredBy;
-    }
+	}
 
 	async add() {
 		const member = this.msg.member;
 		const embed = new MessageEmbed()
 			.setColor('YELLOW')
 			.setTimestamp()
-			.setAuthor(member?.displayName, this.msg.author.displayAvatarURL())
+			.setAuthor(member?.displayName, this.msg.author.displayAvatarURL());
 		if (this.msg.content) {
 			embed.setDescription(`${this.msg.content}\n\n[Jump!](${this.msg.url})`);
-		}
-		else {
+		} else {
 			embed.setDescription(`[Jump!](${this.msg.url})`);
 		}
-		const img = Starboard.findImg(this.msg)
-		if (img) embed.setImage(img)
-			
+		const img = Starboard.findImg(this.msg);
+		if (img) embed.setImage(img);
 
-		let sent = await this.channel.send(`⭐**${this.count}** ${this.msg.channel} `, embed);
+		let sent = await this.channel.send(
+			`⭐**${this.count}** ${this.msg.channel} `,
+			embed
+		);
 		await StarModel.create({
 			_id: this.msg.id,
 			author: this.msg.author.id,
 			guild: this.msg.guild!.id,
-			storageMsg: sent.id, 
+			storageMsg: sent.id,
 			starAmount: this.count,
 			starredBy: this.starredBy,
-		})
+		});
 	}
 
 	static async exists(id: string) {
-		return await StarModel.exists({_id: id})
+		return await StarModel.exists({ _id: id });
 	}
 
 	static emojiEquals(x: any, y: any) {
@@ -77,7 +78,7 @@ export class Starboard {
 		if (/^\d+$/.test(id)) {
 			return client.emojis.cache.get(id);
 		}
-		
+
 		return id;
 	}
 
@@ -85,9 +86,11 @@ export class Starboard {
 		let returnAttachment;
 		const extensions = ['.png', '.jpg', '.jpeg', '.gif', '.webp'];
 
-		const attachment = msg.attachments.find(file => extensions.includes(path.extname(file.url)))
+		const attachment = msg.attachments.find(file =>
+			extensions.includes(path.extname(file.url))
+		);
 		if (attachment) returnAttachment = attachment.url;
-		
+
 		if (!returnAttachment) {
 			const match = msg.content.match(IMAGE_LINK_REGEX);
 			if (match && extensions.includes(path.extname(match[0]))) {
@@ -96,7 +99,5 @@ export class Starboard {
 		}
 
 		return returnAttachment;
-
 	}
-
 }
