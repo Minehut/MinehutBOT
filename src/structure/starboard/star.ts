bba5a0e076959b6c27ee4c71cab46b25d6bc970c
Spiderlogical <thehelpfulspider@gmail.com>
2020-10-13 23:37:44 -0700

    prettify and minor changes

--- src/structure/starboard/star.ts
+++ src/structure/starboard/star.ts
@@ -1,26 +1,30 @@
-import { TextChannel } from "discord.js";
-import { MessageEmbed } from "discord.js";
-import { Message } from "discord.js";
-import { MinehutClient } from "../../client/minehutClient";
-import { StarModel } from "../../model/star";
-import { IMGUR_LINK_REGEX } from "../../util/constants";
+import { TextChannel } from 'discord.js';
+import { MessageEmbed } from 'discord.js';
+import { Message } from 'discord.js';
+import path from 'path';
+import { MinehutClient } from '../../client/minehutClient';
+import { StarModel } from '../../model/star';
+import { IMGUR_LINK_REGEX } from '../../util/constants';
 
 export interface StarboardData {
 	msg: Message;
 	channel: TextChannel;
 	count: number;
+	starredBy: string[];
 }
 
 export class Starboard {
 	channel: TextChannel;
 	msg: Message;
 	count: number;
+	starredBy: string[];
 
 	constructor(data: StarboardData) {
 		this.channel = data.channel;
 		this.msg = data.msg;
 		this.count = data.count;
-    }
+		this.starredBy = data.starredBy;
+	}
 
 	async add() {
 		const member = this.msg.member;
@@ -30,31 +34,31 @@ export class Starboard {
 		const embed = new MessageEmbed()
 			.setColor('YELLOW')
 			.setTimestamp()
-			.setAuthor(member?.displayName, this.msg.author.displayAvatarURL())
-			.setDescription(`${content}\n\n[Jump!](${this.msg.url})`);
-		if (matches) {
-			const link = matches[0];
-			content = this.msg.content.replace(link, '');
-			embed.setImage(link);
+			.setAuthor(member?.displayName, this.msg.author.displayAvatarURL());
+		if (this.msg.content) {
+			embed.setDescription(`${this.msg.content}\n\n[Jump!](${this.msg.url})`);
+		} else {
+			embed.setDescription(`[Jump!](${this.msg.url})`);
 		}
-		else if (this.msg.attachments) {
-			let firstAttached = this.msg.attachments.first();
-			if (firstAttached?.width) {
-				embed.setImage(firstAttached.url);
-			}
-		}
+		const img = Starboard.findImg(this.msg);
+		if (img) embed.setImage(img);
 
+		let sent = await this.channel.send(
+			`⭐**${this.count}** ${this.msg.channel} `,
+			embed
+		);
 		await StarModel.create({
 			_id: this.msg.id,
 			author: this.msg.author.id,
 			guild: this.msg.guild!.id,
-			starAmount: this.count
-		})
-		await this.channel.send(`⭐**${this.count}** ${this.msg.channel} `, embed);
+			storageMsg: sent.id,
+			starAmount: this.count,
+			starredBy: this.starredBy,
+		});
 	}
 
 	static async exists(id: string) {
-		return await StarModel.exists({_id: id})
+		return await StarModel.exists({ _id: id });
 	}
 
 	static emojiEquals(x: any, y: any) {
@@ -77,8 +81,26 @@ export class Starboard {
 		if (/^\d+$/.test(id)) {
 			return client.emojis.cache.get(id);
 		}
-		
+
 		return id;
 	}
 
+	static findImg(msg: Message) {
+		let returnAttachment;
+		const extensions = ['.png', '.jpg', '.jpeg', '.gif', '.webp'];
+
+		const attachment = msg.attachments.find(file =>
+			extensions.includes(path.extname(file.url))
+		);
+		if (attachment) returnAttachment = attachment.url;
+
+		if (!returnAttachment) {
+			const match = msg.content.match(IMGUR_LINK_REGEX);
+			if (match && extensions.includes(path.extname(match[0]))) {
+				returnAttachment = match[0];
+			}
+		}
+
+		return returnAttachment;
+	}
 }
