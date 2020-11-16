027d577f86b1853249c06843dd365ec7fa08c1bb
Sponk <64669537+ItsSponk@users.noreply.github.com>
2020-11-16 01:12:18 -0700

    Update ban.ts

--- src/structure/action/ban.ts
+++ src/structure/action/ban.ts
@@ -82,7 +82,7 @@ export class BanAction extends Action {
 					humanizeDuration(this.duration, { largest: 3, round: true })
 				)
 				.addField('Expires', prettyDate(this.expiresAt))
-				.addField('Punished unfairly? [Appeal here](https://forums.minehut.com/application/form/5-player-appeals/)')
+				.addField('Punished unfairly? [Appeal here](https://forums.minehut.com/application/form/5-player-appeals/)', true)
 				.setTimestamp();
 			await this.target.send(embed);
 		} catch (err) {}
