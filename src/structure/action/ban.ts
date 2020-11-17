d7fc92ea3eeeba050aa1a903fc047ea4340a85bb
Sponk <64669537+ItsSponk@users.noreply.github.com>
2020-11-17 16:35:45 -0700

    Moved link to footer

--- src/structure/action/ban.ts
+++ src/structure/action/ban.ts
@@ -82,8 +82,8 @@ export class BanAction extends Action {
 					humanizeDuration(this.duration, { largest: 3, round: true })
 				)
 				.addField('Expires', prettyDate(this.expiresAt))
-				.addField('Punished unfairly? [Appeal here](https://forums.minehut.com/application/form/5-player-appeals/)', true)
-				.setTimestamp();
+				.setTimestamp()
+				.setFooter('forums.minehut.com', 'https://i.imgur.com/dnyXrKy.png');
 			await this.target.send(embed);
 		} catch (err) {}
 	}
