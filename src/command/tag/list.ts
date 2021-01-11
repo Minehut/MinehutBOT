24145978de3ae85eb6636ae534afe77a1c6ab3dd
honkling <62254220+honkling@users.noreply.github.com>
2021-01-10 21:19:13 -0600

    Remove any type from tag-list

--- src/command/tag/list.ts
+++ src/command/tag/list.ts
@@ -22,16 +22,16 @@ export default class TagListCommand extends MinehutCommand {
 		const embed = new MessageEmbed();
 		embed.setColor('ORANGE');
 		embed.setTitle(`Showing ${tags.length} tags`);
-		const fields: any = {};
+		const fields: { [ key: string ] : typeof tags[0][] } = {};
 		for (const tag of tags) {
 			if (!fields[tag.section || 'Uncategorized'])
 				fields[tag.section || 'Uncategorized'] = [];
 			fields[tag.section || 'Uncategorized'].push(tag);
 		}
 		for (const name in fields) {
-			const field = fields[name];
-			const mapped = truncate(
-				field.map((t: any) => `\`${t.name}\``).join(', '),
+			const field: object[] = fields[name];
+			const mapped: string = truncate(
+				field.map((t: { name? : string }) => `\`${t.name}\``).join(', '),
 				{ length: 512 }
 			);
 			embed.addField(name, mapped, true);
