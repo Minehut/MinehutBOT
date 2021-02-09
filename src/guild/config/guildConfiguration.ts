8f4c45f1ccfc8fe275433d1e49cf8ee56ead2c51
Sniped <facto@facto.club>
2021-02-09 16:02:45 -0500

    lockdown command

--- src/guild/config/guildConfiguration.ts
+++ src/guild/config/guildConfiguration.ts
@@ -8,6 +8,7 @@ import { AutoReactConfiguration } from './feature/autoReact';
 import { BoosterPassConfiguration } from './feature/boosterPass';
 import { HastebinConversionConfiguration } from './feature/hastbinConversion';
 import { GithubIssueReferenceConfiguration } from './feature/githubIssue';
+import { ChannelLockdownConfiguration } from './feature/channelLockdown';
 
 // The GuildConfiguration can have settings about permissions, log channels, disabled features, etc.
 export interface GuildConfiguration {
@@ -25,5 +26,6 @@ export interface GuildConfiguration {
 		boosterPass?: BoosterPassConfiguration;
 		hastebinConversion?: HastebinConversionConfiguration;
 		githubIssue?: GithubIssueReferenceConfiguration;
+		channelLockdown?: ChannelLockdownConfiguration;
 	};
 }
