c52938ac650a8e3f0403305163ecf79e11d91c41
tarna <46247728+tarna@users.noreply.github.com>
2021-10-12 15:06:01 -0400

    use new prettyDate function

--- src/command/mod/case/search.ts
+++ src/command/mod/case/search.ts
@@ -73,9 +73,9 @@ export default class CaseSearchCommand extends MinehutCommand {
 							{ round: true, largest: 3 }
 					  )}`
 					: null,
-				`- **__Date:__** ${prettyDate(c.createdAt, false)} (${prettyDate(c.createdAt)})`,
+				`- **__Date:__** ${prettyDate(c.createdAt)}`,
 				c.expiresAt.getTime() > -1
-					? `- **__Expires:__** ${prettyDate(c.expiresAt, false)} (${prettyDate(c.createdAt)})`
+					? `- **__Expires:__** ${prettyDate(c.expiresAt)}`
 					: null,
 			]
 				.filter(i => i !== null)
